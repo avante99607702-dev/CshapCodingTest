5242a8c837d17ce586b1e47b58636969bde63810
전민경 <avante99607702@gmail.com>
2026-01-09 23:45:04 +0900

    [level 0] Title: 양꼬치, Time: 0.17 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        
+        int freeDrink = n / 10;
+        answer = 12000*n + 2000*(k-freeDrink);
+
+        return answer;
+    }
+}
