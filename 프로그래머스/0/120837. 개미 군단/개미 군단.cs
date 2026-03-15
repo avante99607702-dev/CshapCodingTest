b7f3544dfe2df16d570d2653f273041b7557aed1
전민경 <avante99607702@gmail.com>
2026-03-15 23:43:06 +0900

    [level 0] Title: 개미 군단, Time: 0.16 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120837. 개미 군단/개미 군단.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int solution(int hp) {
+        int count = 0;
+        
+        count+=hp/5;
+        hp%=5;
+        
+        count+=hp/3;
+        hp%=3;
+        
+        count+=hp;
+        
+        return count;
+    }
+}
