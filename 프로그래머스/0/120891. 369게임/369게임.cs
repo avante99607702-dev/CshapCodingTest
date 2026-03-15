abebfd4d4d377bc0ce44b03604ae32b10df7862d
전민경 <avante99607702@gmail.com>
2026-03-15 23:29:02 +0900

    [level 0] Title: 369게임, Time: 0.17 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120891. 369게임/369게임.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class Solution {
+    public int solution(int order) {
+        int count =0;
+        
+        while(order>0){
+            int digit = order%10;
+            
+            if(digit==3 || digit==6 || digit==9){
+                count++;
+            }
+            order/=10;
+        }
+        return count;
+    }
+}
