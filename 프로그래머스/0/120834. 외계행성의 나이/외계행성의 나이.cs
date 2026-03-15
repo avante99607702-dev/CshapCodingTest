e6bd4eb8f2feb955faea14bcf430e64cab8db7ac
전민경 <avante99607702@gmail.com>
2026-03-15 23:35:15 +0900

    [level 0] Title: 외계행성의 나이, Time: 0.31 ms, Memory: 30.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120834. 외계행성의 나이/외계행성의 나이.cs
@@ -0,0 +1,18 @@
+using System;
+
+public class Solution {
+    public string solution(int age) {
+        string answer = "";
+        
+        while(age>0)
+        {
+            int digit = age % 10;
+            char ch = (char)('a' + digit);
+            
+            answer = ch+answer;
+            age /=10;
+        }
+        
+        return answer;
+    }
+}
